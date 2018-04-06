292b00270af42ec5b2659ac03775fd091d199c99
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-06 23:25:46 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,3 @@
 def position_taken?(board, index)
-if  (board[index] == " " || board[index] == "" || board[index] == nil)
-  false
-else (board[index] == "X" || board[index] == "O")
-  true
-end
+  board[index] == "X" || board[index] == "O"
 end
